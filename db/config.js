cf2ad3eee051fbbcb7e6480b77984e6bf68c3737
jolean lim <joleanflima@outlook.com>
2021-04-10 15:44:14 -0400

    cofig.js

---
+++ db/config.js
@@ -0,0 +1,22 @@
+const options = {
+    query: (e) => {
+        console.log(e.query);
+    }
+}
+
+const pgp = require ('pg-promise')(options);
+
+function setBatabase() {
+    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+        return pgp({
+            database: 'dez_library',
+            port: 5432,
+            host: 'localhost',
+        })
+    } else if (process.env.NODE_ENV === 'production') {
+        return pgp(process.env.DATABASE_URL);
+    }
+}
+
+const db = setDatabase();
+module.exports = db;
